57e1dbdae407dcb565b7d06bac3a95fcd593e783
Habibafernas <habibafernas@gmail.com>
2023-10-31 15:31:57 -0700

    a

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+for i in range(0, 90):
+    if i in range(10, 12) || i in range(20, 23) || i in range(30, 34)|| i in range(40,45) || i in range(50, 56) || i in range(60, 67) || i in range(70, 78) || i in range(80, 89):
+    
+    elif i == 89:
+        print("{:02}".format(i))
+    else:
+        print("{:02}".format(i), end=",")
+    
+
+        
