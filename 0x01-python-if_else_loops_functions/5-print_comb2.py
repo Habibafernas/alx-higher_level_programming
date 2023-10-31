c047ffb9e31ee304f4cf16dae02da9110e8ccd9d
Habibafernas <habibafernas@gmail.com>
2023-10-31 15:03:30 -0700

    a

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(0, 100):
-    if i in range(0, 10):
+    if i in range(0, 99):
         print("{:02}".format(i), end=", ")
     else:
-        print("{}".format(i), end=", ")
+        print("{}".format(i))
