7f59b067bc8187a1fa1f017bef19463af54ea366
Habibafernas <habibafernas@gmail.com>
2023-10-31 15:43:03 -0700

    a

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(0, 90):
-    if i not in range(10, 12) || i in range(20, 23) || i in range(30, 34)|| i in range(40,45) || i in range(50, 56) || i in range(60, 67) || i in range(70, 78) || i in range(80, 89):
+    if i not in range(10, 12) or i in range(20, 23) or i in range(30, 34) or i in range(40,45) or i in range(50, 56) or i in range(60, 67) or i in range(70, 78) or i in range(80, 89):
         print("{:02}".format(i), end=",")
     elif i == 89:
         print("{:02}".format(i))
