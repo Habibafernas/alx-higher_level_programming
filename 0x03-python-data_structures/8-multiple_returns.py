1ae778faf31032bc24ef5ef9f276333ead552136
Habibafernas <habibafernas@gmail.com>
2023-11-05 08:47:28 -0800

    a

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,7 +3,8 @@ def multiple_returns(sentence):
     new = ()
     if len(sentence) == 0:
         new = 0, "None"
-    l = len(sentence)
-    f = sentence[0]
-    new = l, f
+    else:
+        l = len(sentence)
+        f = sentence[0]
+        new = l, f
     return (new)
