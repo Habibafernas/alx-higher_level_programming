bdb60a25ae4feb5ee74b2b69f1574b9d0b65b69e
Habibafernas <habibafernas@gmail.com>
2023-11-08 15:13:51 -0800

    a

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -4,5 +4,5 @@ d = []
 def square_matrix_simple(matrix=[]):
     for row in matrix:
         for c in row:
-            d[c][row] = (matrix[c]) ** 2
+            d[c]= (matrix[c]) ** 2
     return d
