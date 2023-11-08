12232756e51c371b72cf5ff6f11982296c1efe5a
Habibafernas <habibafernas@gmail.com>
2023-11-08 15:27:20 -0800

    a

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -5,6 +5,8 @@ def search_replace(my_list, search, replace):
     new = mylist
 
 
-    new[search] = replace
+    for i in range mylist:
+        if mylist[i] == search:
+            new[i] = replace
     return new
 
