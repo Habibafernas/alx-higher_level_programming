bb24fb45a9552877a9f70db92635d4244fee72d7
Habibafernas <habibafernas@gmail.com>
2023-11-12 12:06:02 -0800

    a

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+def multiply_by_2(a_dictionary):
+    b = {}
+
+    for key, value in a_dictionary.items():
+        b.update({key: (value * 2)})
+    return b
+            
