62da63ad11975be70c3100cf024bb2b4dbb20c73
Habibafernas <habibafernas@gmail.com>
2023-11-21 13:17:55 -0800

    a

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -5,6 +5,7 @@
 
 class Square:
     """Represent a square."""
+
     def __init__(self, size=0):
         """initialises a square"""
         self.size = size
@@ -20,7 +21,7 @@ class Square:
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
-            self.__size = value
+        self.__size = value
 
     def area(self):
         return (self.__size * self.__size)
