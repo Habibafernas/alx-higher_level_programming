f293b69059b0c92787161c6b372cad61aabb38d7
Habibafernas <habibafernas@gmail.com>
2023-11-21 11:46:59 -0800

    a

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+
+"""Defines a class"""
+
+
+class Square:
+    """initialises a square"""
+
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """calculates the area"""
+
+        return (self.__size * self.size)
