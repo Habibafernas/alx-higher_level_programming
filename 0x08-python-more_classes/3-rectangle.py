072b302d49abdca82e961a28b8a2ae701c094076
Habibafernas <habibafernas@gmail.com>
2023-11-27 15:32:08 -0800

    a

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -55,5 +55,6 @@ class Rectangle:
     for i in range(self.__height):
         for j in range(self.__width):
             rec.append('#')
-        print("\n")
+        if i != self.__height - 1:
+                rec.append("\n")
     return ("".join(rec))
