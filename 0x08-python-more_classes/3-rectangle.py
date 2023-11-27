db21b8a02156b768787f57cd37d14d2ead7bb5b7
Habibafernas <habibafernas@gmail.com>
2023-11-27 15:37:03 -0800

    a

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -50,10 +50,13 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return (0)
         return ((self.__width * 2) + (self.__height * 2))
+    def __str__(self):
+        if self.__width == 0 or self.__height == 0:
+                return ("")
 
-    rec = []
-    for i in range(self.__height):
-        [rec.append('#') for j in range(self.__width)]
-        if i != self.__height - 1:
-            rec.append("\n")
-    return ("".join(rec))
+        rec = []
+        for i in range(self.__height):
+            [rec.append('#') for j in range(self.__width)]
+            if i != self.__height - 1:
+                rec.append("\n")
+        return ("".join(rec))
