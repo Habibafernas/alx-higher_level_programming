eb1d74f9b25590a342053b95a095e7fd731801ee
Habibafernas <habibafernas@gmail.com>
2023-11-29 09:02:04 -0800

    a

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,8 +1,23 @@
 #!/usr/bin/python3
+"""Module for matrix_divided method."""
 
 
-def (matrix_divided(matrix, div):
-    if not isinstance(matrix, list) or matrix == [] or not all(isinstance(row, list) for row in matrix) or
+def matrix_divided(matrix, div):
+    """Divides all elements of matrix by div.
+    Args:
+        matrix: List of lists containing int or float
+        div: number to divide matrix by
+    Returns:
+        list: List of lists representing divided matrix.
+    Raises:
+        TypeError: If matrix is not list of lists containing int or float.
+        TypeError: If sublists are not all same size.
+        TypeError: If div is not int or float.
+        ZeroDivisionError: If div is zero.
+    """
+
+    if (not isinstance(matrix, list) or matrix == [] or 
+            not all(isinstance(row, list) for row in matrix) or
             not all((isinstance(ele, int) or isinstance(ele, float))
                     for ele in [num for row in matrix for num in row])):
         raise TypeError("matrix must be a matrix (list of lists) of "
@@ -17,3 +32,6 @@ def (matrix_divided(matrix, div):
         raise ZeroDivisionError("division by zero")
 
     return ([list(map(lambda x: round(x / div, 2), row)) for row in matrix])
+    if __name__ == "__main__":
+        import doctest
+        doctest.testfile("tests/2-matrix_divided.txt")
