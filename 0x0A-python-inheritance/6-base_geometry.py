3e53cf5bff8679cdcb44b4d0b4a1fcc811552cab
Habibafernas <habibafernas@gmail.com>
2023-12-04 14:00:53 -0800

    a

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""defines a class"""
+
+
+class BaseGeometry:
+    """defines a class"""
+
+    def area(self):
+        """calculates the area"""
+        raise Exception("area() is not implemented")
