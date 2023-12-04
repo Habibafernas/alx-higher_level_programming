0b8e3c793dd2f81d237b8bbfc704ac542122262b
Habibafernas <habibafernas@gmail.com>
2023-12-04 13:53:59 -0800

    a

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -4,7 +4,7 @@
 
 def inherits_from(obj, a_class):
     """chaecks the type of an obj"""
-    if issubclass(type(obj) == a_class) and type(obj) != a_class:
+    if issubclass(type(obj) ,a_class) and type(obj) != a_class:
         return True
     else:
         return False
