2aca873549ff589a9651c288aa110bee3ee116cc
Habibafernas <habibafernas@gmail.com>
2023-12-04 13:53:05 -0800

    a

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""defines a class"""
+
+
+def inherits_from(obj, a_class):
+    """chaecks the type of an obj"""
+    if issubclass(type(obj) == a_class) and type(obj) != a_class:
+        return True
+    else:
+        return False
