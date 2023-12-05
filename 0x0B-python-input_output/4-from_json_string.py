1a642ddb6db910178e988553bc1f94b4515f6800
Habibafernas <habibafernas@gmail.com>
2023-12-05 13:09:02 -0800

    a

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 # 6-from_json_string.py
 """defines a class"""
-
 import json
 
+
 def from_json_string(my_str):
-     """returns an object represented by a JSON string"""
+    """returns an object represented by a JSON string"""
     return json.loads(my_str)
