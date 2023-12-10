42c93e500b8e9c62bfbfb1bc56557eec644532ee
Habibafernas <habibafernas@gmail.com>
2023-12-10 14:04:55 -0800

    a

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Defines a base model class."""
-
+import json
 
 class Base:
     """defines a base model"""
@@ -22,3 +22,8 @@ class Base:
         if list_dictionaries is None or list_dictionaries = []:
             return "[]"
         return json.dumps(list_dictionaries)
+    
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """that writes the JSON string representation of list_objs"""
+        
