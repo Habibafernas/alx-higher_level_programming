e890f29b6c4e25329582f9a8da89649e197970ae
Habibafernas <habibafernas@gmail.com>
2023-12-27 14:49:33 -0800

    A

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" function that prints a text with 2 new lines after characters"""
+
+
+def text_indentation(text):
+    """ function that prints a text with 2 new lines after characters"""
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    c == 0
+    while c < len(text):
+        print(text[c], end="")
+        if text[c] = "\n" or text[c] in ".?:":
+            print("\n")
+        c += 1
+
