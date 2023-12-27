193c430325da2a0f2aac76284f4d469665ccdc21
Habibafernas <habibafernas@gmail.com>
2023-12-27 14:07:01 -0800

    A

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""function that prints a square with the character #"""
+
+
+def print_square(size):
+    """function that prints a square with the character #"""
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+
+    for i in range(size):
+        [print("#", end="") for j in range(size)]
+        print("")
