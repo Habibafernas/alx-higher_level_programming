93ec00d3d5b0a6f155cd7edb8d277679536fed94
Habibafernas <habibafernas@gmail.com>
2024-02-12 23:51:51 +0200

    a

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+if (Process.argv.length === 2)
+{
+    console.log = ('No argument');
+}
+else if (Process.argv.length === 3)
+{
+    console.log = ('Argument found');
+}
+else
+{
+    console.log = ('Argument found');
+}
