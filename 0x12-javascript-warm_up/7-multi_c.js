e80a413963a26999a467f76e87f21aabfeaffc59
Habibafernas <habibafernas@gmail.com>
2024-02-14 22:02:44 +0200

    a

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+if (parseInt(process.argv[2])) {
+    const x = parseInt(process.argv[2]);
+    let i = 0;
+    while (i < x) {
+        console.log('C is fun');
+        i++;
+    }
+} else {
+    console.log('Missing number of occurrences');
+}
