9fefbc249f6713259b91d3dbe59860d96de41587
HabibaFernas <habibafernas@gmail.com>
2024-03-04 21:13:57 +0200

    a

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+a Python script that takes in a URL and an email,
+sends a POST request to the passed URL with the email as a parameter
+"""
+import sys
+import urllib.request
+import urllib.parse
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    v = {"email": sys.argv[2]}
+    data = urllib.parse.urlencode(v).encode("ascii")
+
+    request = urllib.request.Request(url ,data)
+    with urllib.request.urlopen(request) as response:
+        print(response.read().decode("utf-8"))
