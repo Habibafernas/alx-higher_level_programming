9a24ad88c83052e7c92f6794378992a5a543bc7d
HabibaFernas <habibafernas@gmail.com>
2024-03-04 21:16:09 +0200

    a

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """
-a Python script that takes in a URL and an email,
-sends a POST request to the passed URL with the email as a parameter
+a Python script that takes in:
+a URL and an email
+sends a POST request to the passed URL
+with the email as a parameter
 """
 import sys
 import urllib.request
