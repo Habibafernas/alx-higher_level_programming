c84fe98ff341dc62f0049e20340b6bfcda239125
HabibaFernas <habibafernas@gmail.com>
2024-03-27 01:42:20 +0200

    a

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+const request = require('request');
+const num = process.argv[2];
+const API_URL = 'https://swapi-api.alx-tools.com/api/films/:id';
+
+request(API_URL + num, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else if (response.statusCode === 200) {
+    const responseJSON = JSON.parse(body);
+    console.log(responseJSON.title);
+  } else {
+    console.log('Error code: ' + response.statusCode);
+  }
+});
