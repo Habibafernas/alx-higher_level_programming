25a6e6ad6ae50b03cb8bd87730f86c382bdce145
HabibaFernas <habibafernas@gmail.com>
2024-03-28 00:57:18 +0200

    a

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
-const request = require('request');
+const req = require('request');
 const url = 'https://swapi-api.hbtn.io/api/films/';
-request.get(url + process.argv[2], function (err, request, body) {
+req.get(url + process.argv[2], function (err, res, body) {
   if (err) {
     console.log(err);
   }
   const data = JSON.parse(body);
   const dd = data.characters;
   for (const i of dd) {
-    request.get(i, function (err, request, body1) {
+    req.get(i, function (err, res, body1) {
       if (err) {
         console.log(err);
       }
